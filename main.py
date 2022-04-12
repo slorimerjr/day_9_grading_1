ca1ed1e29e49e6d55e1a61e4d992ac33491c0a82
Sam Lorimer <slorimerjr@gmail.com>
2022-04-12 21:10:47 +0000

    added teacher solution

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ for student in student_scores:
     student_grades[student] = "Acceptable"
   else:
     student_grades[student] = "Fail"
+
+# teacher solution
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
       
 # 🚨 Don't change the code below 👇
 print(student_grades)
